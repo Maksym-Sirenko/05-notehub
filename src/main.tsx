c6a699350aec05c5873621d244e32464d012b415
Maksym-Sirenko <toyotasan@gmail.com>
2025-10-09 14:30:01 +0300

    litle-more-and some fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+// import styles from "./index.css";
 import App from "./components/App/App";
 import "modern-normalize/modern-normalize.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
